244976de95d5a13f2aa5c5f26e3b4b53a16ab925
Hans.W <hanswang2012@gmail.com>
2016-07-16 00:15:47 +1000

    re-use much of mainJs, passing through brand -> pages to accumulate items and write to json

--- parse.js
+++ parse.js
@@ -1,26 +1,64 @@
 var cheerio = require('cheerio'),
     fs = require('fs'),
-    list = [],
-    brand = 'cw';
+    async = require('async'),
+    sleep = require('sleep'),
+    brands = [
+                {name: 'cw', supId: 10, count: 72},
+                {name: 'coles', supId: 22, count: 11},
+                {name: 'wt', supId: 17, count: 3},
+                {name: 'priceline', supId: 25, count: 3},
+                {name: 'aldi', supId: 14, count: 3},
+                {name: 'woolies', supId: 23, count: 1},
+                {name: 'myer', supId: 24, count: 1}
+            ];
 
-fs.readFile(brand + '/01.html', function(err, html) {
-    var $ = cheerio.load(html),
+async.eachOfSeries(brands, function(brand, id, brand_callback) {
 
-    $('body div.contain ul.goods_list li').each(function(i, ele){
-        var item = { title : "", img : "", price : "", ref_price : "", filename : "" };
-        item.img = $(ele).find('div.img img').attr('src');
+    var htmlParserWrapper = function (id, callback) {
+        var p = String('0000'+(id+1)).slice(-4),
+            fn = 'content/html/' + brand.name + p + '.htm',
+            list = [];
 
-        item.title = $(ele).find('div.txt .tit span.imp').text();
-        item.price = $(ele).find('div.txt .price span.price-span').last().text();
-        item.ref_price = $(ele).find('div.txt .ops span.pri').text();
-        item.filename = String('0000'+(i+1)).slice(-4) + '.jpg';
+        fs.readFile(fn, function(err, html) {
+            var $ = cheerio.load(html);
 
-        if (item.title.length > 0) {
-            list.push(item);
-        }
-    })
+            $('body div.contain ul.goods_list li').each(function(i, ele){
+                var item = { title : "", img : "", price : "", ref_price : "", filename : "" };
+                item.img = $(ele).find('div.img img').attr('src');
 
-    fs.writeFile(brand + '/index.json', JSON.stringify(list, null, 4), function(err) {
-        console.log('File successfully written');
-    })
+                item.title = $(ele).find('div.txt .tit span.imp').text();
+                item.price = $(ele).find('div.txt .price span.price-span').last().text();
+                item.ref_price = $(ele).find('div.txt .ops span.pri').text();
+                item.filename = String('0000'+(i+1)).slice(-4) + '.jpg';
+
+                if (item.title.length > 0) {
+                    list.push(item);
+                }
+            });
+
+            callback(null, list);
+        });
+    };
+
+    async.timesSeries(brand.count, function (n, next) {
+        htmlParserWrapper(n, function (err, i_list) {
+            console.log('list length: ' + i_list.length);
+            next(err, i_list);
+        });
+    }, function (err, lists) {
+        console.log(brand.name + ' has all parsed for ' + brand.count + ' htms');
+
+        var merged = [].concat.apply([], lists);
+        fs.writeFile('content/json/' + brand.name + '.json', JSON.stringify(merged, null, 4), function(err) {
+            console.log('File ' + brand.name + '.json' + ' successfully written');
+            sleep.sleep(1);
+            console.log('switching to next brand');
+            brand_callback();
+        });
+    });
+
+}, function (err) {
+    if (err) {
+        console.log(err.message);
+    }
 });
